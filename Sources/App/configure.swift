64e0fde860ad431fecd8df09f18e711077435b3a
aaronium112 <aaron@sphericalwave.com>
2020-05-02 18:55:58 -0300

    init

---
+++ Sources/App/configure.swift
@@ -0,0 +1,23 @@
+import Fluent
+import FluentSQLiteDriver
+import Vapor
+import Leaf
+
+// Called before your application initializes.
+public func configure(_ app: Application) throws
+{
+    // Serves files from `Public/` directory
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    // Configure Leaf
+    app.views.use(.leaf)
+    app.leaf.cache.isEnabled = app.environment.isRelease
+
+    // Configure SQLite database
+    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+
+    // Configure migrations
+    app.migrations.add(CreateTodo())
+    
+    try routes(app)
+}
---
+++ Sources/App/routes.swift
@@ -0,0 +1,46 @@
+import Fluent
+import Vapor
+
+func routes(_ app: Application) throws
+{
+    app.get { req -> EventLoopFuture<View> in
+        let context = Context(index: .init(title: "My page", description: "This is my Page"),
+                              page: .init(content: "Welcome to my page!"))
+        return req.view.render("page", context)
+    }
+    
+    app.get("srvSnglChoice") { req -> EventLoopFuture<View> in
+        let ndx = Index(title: "srvSnglChoice", description: "What hurts the most?")
+        let pg = Page(content: "Fingers, Toes")
+        let context = Context(index: ndx, page: pg)
+        return req.view.render("srvSnglChoice", context)
+    }
+    
+    app.get("question2") { req -> EventLoopFuture<View> in
+        return req.view.render("question2")
+    }
+    
+    app.get("hello") { req in return "Hello, Aaron!" }
+    
+    let todoController = TodoController()
+    app.get("todos", use: todoController.index)
+    app.post("todos", use: todoController.create)
+    app.on(.DELETE, "todos", ":todoID", use: todoController.delete)
+}
+
+struct Context: Codable
+{
+    var index: Index
+    var page: Page
+}
+
+struct Index: Codable
+{
+    var title: String
+    var description: String
+}
+
+struct Page: Codable
+{
+    var content: String
+}
