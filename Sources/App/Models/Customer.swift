08cad91a1359bb037cec6f44dfead124af8feda5
aaronium112 <aaron@sphericalwave.com>
2020-06-24 16:29:54 -0300

    still working on OAuth

--- Sources/App/Models/Customer.swift
+++ Sources/App/Models/Customer.swift
@@ -8,35 +8,35 @@
 import Fluent
 import Vapor
 
-final class Customer: Model, Content {
-    // Name of the table or collection.
-    static let schema = "customers"
-
-    @ID(key: .id) var id: UUID?   //TODO: Be Immutable
-
-    @Field(key: "name") var name: String    //TODO: Be Immutable
-
-    init() { }  //TODO: Disagree
-
-    init(id: UUID? = nil, name: String) {
-        self.id = id
-        self.name = name
-    }
-}
-
-
-struct CustomerMgrt: Migration
-{
-    // Prepares the database for storing Customer models.
-    func prepare(on database: Database) -> EventLoopFuture<Void> {
-        database.schema("customers")
-            .id()
-            .field("name", .string)
-            .create()
-    }
-
-    // Optionally reverts the changes made in the prepare method.
-    func revert(on database: Database) -> EventLoopFuture<Void> {
-        database.schema("customers").delete()
-    }
-}
+//final class Customer: Model, Content {
+//    // Name of the table or collection.
+//    static let schema = "customers"
+//
+//    @ID(key: .id) var id: UUID?   //TODO: Be Immutable
+//
+//    @Field(key: "name") var name: String    //TODO: Be Immutable
+//
+//    init() { }  //TODO: Disagree
+//
+//    init(id: UUID? = nil, name: String) {
+//        self.id = id
+//        self.name = name
+//    }
+//}
+//
+//
+//struct CustomerMgrt: Migration
+//{
+//    // Prepares the database for storing Customer models.
+//    func prepare(on database: Database) -> EventLoopFuture<Void> {
+//        database.schema("customers")
+//            .id()
+//            .field("name", .string)
+//            .create()
+//    }
+//
+//    // Optionally reverts the changes made in the prepare method.
+//    func revert(on database: Database) -> EventLoopFuture<Void> {
+//        database.schema("customers").delete()
+//    }
+//}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,72 +7,22 @@ import ImperialGitHub
 
 public func configure(_ app: Application) throws
 {
-    // Serves files from `Public/` directory
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
-    // Configure Leaf
     app.views.use(.leaf)
     app.leaf.cache.isEnabled = app.environment.isRelease
 
-    // Configure SQLite database
-    //app.databases.use(.sqlite(.memory), as: .sqlite)
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
-    //app.databases.use(.postgres(hostname: "localhost", username: "vapor", password: "vapor", database: "vapor"), as: .psql)
-
-
-    // Configure migrations
-    //app.migrations.add(CreateTodo())
-    app.migrations.add(CustomerMgrt())
-    //try app.autoMigrate().wait() //for in memory //Some issue here if i leave this on
+    app.databases.use(.sqlite(.file("db.sqlite") ), as: .sqlite)
     
-    app.sessions.use(.fluent(.sqlite)) //.sqlite
-    app.middleware.use(app.sessions.middleware)
-    app.sessions.configuration.cookieName = "PainFreeJiujitsu"
-    // Configures cookie value creation.
-    app.sessions.configuration.cookieFactory = { sessionID in
-        .init(string: sessionID.string, isSecure: true)
-    }
     app.sessions.use(.fluent)
-    //app.sessions.use(.fluent(.sqlite))
-    app.migrations.add(SessionRecord.migration)  //how do i run migrations?
+    app.migrations.add(SessionRecord.migration)
+    app.middleware.use(app.sessions.middleware)
     
-    app.get("set", ":value") { req -> HTTPStatus in
-        print(req.description)
-        for (cookie, value) in req.cookies.all {
-            print("\(cookie) : \(value)")
-            let cD = cookie.description
-            if cD == "vapor-session" { print("i see you've played knify Spoony before")}
-        }
-        let input = req.parameters.get("value")
-        var data = req.session.data
-        data["name"] = input
-        print("sessionData name \(data["name"]!)")
-        return .ok
-    }
-    
-    app.get("get") { req -> String in
-        print(req.description)
-        let sNm = req.session.data["name"] ?? "no name assigned"
-        print("get sessionData name \(sNm)")
-        return sNm //req.session.data["name"] ?? "n/a"
-    }
-    
-    app.get("del") { req -> HTTPStatus in
-        print(req.description)
-        let sNm = req.session.data["name"] ?? "no name assigned"
-        print("del sessionData name \(sNm)")
-        req.session.destroy()
-        return .ok
-    }
-    
-    app.post("customer") { req -> EventLoopFuture<Customer> in
-        for (cookie, value) in req.cookies.all { print("\(cookie) : \(value)")}
-        let cstmr = try req.content.decode(Customer.self)
-        return cstmr.create(on: req.db).map { cstmr }
-    }
-    
-    app.get("customer") { req in
-        Customer.query(on: req.db).all()
+    try app.oAuth(from: GitHub.self, authenticate: "github", callback: "") { (request, token) in
+        print("auth token: \(token)")
+        print("request: \(request.description)")
+        return request.eventLoop.makeSucceededFuture(Response())
+        //return request.eventLoop.future(request.redirect(to: "/"))
     }
 
     try! app.register(collection: LandingPg())
@@ -81,38 +31,9 @@ public func configure(_ app: Application) throws
     try! app.register(collection: DemographicPg())
     try! app.register(collection: EmailPg())
     try! app.register(collection: AgeQPg())
-
-    //try! app.register(collection: HelloPg())
-    //try! app.register(collection: PinkBxPg())
     try! app.register(collection: SalesPg())
     try! app.register(collection: DeepDivePg())
-    //try! app.register(collection: HomePg())
-    //try! app.register(collection: YogurtPg())
-    
-//    try! app.register(collection: ImperialAPI())
-//    
-   // try! app.oAuth(from: Google.self, authenticate: "google", callback: "http://localhost:8080/google-complete", redirect: "/")
-    
-    //try! app.oAuth(from: GitHub.self, authenticate: "github", callback: "gh-auth-complete", redirect: "/")
-//    app.oAuth(from: <#T##FederatedService.Protocol#>, authenticate: <#T##String#>, authenticateCallback: <#T##((Request) throws -> (EventLoopFuture<Void>))?##((Request) throws -> (EventLoopFuture<Void>))?##(Request) throws -> (EventLoopFuture<Void>)#>, callback: <#T##String#>, scope: <#T##[String]#>, completion: <#T##(Request, String) throws -> EventLoopFuture<ResponseEncodable>#>)
-
-    //    app.oAuth(from: <#T##FederatedService.Protocol#>, authenticate: <#T##String#>, callback: <#T##String#>, redirect: <#T##String#>)
-    
-    //app.oAuth(from: <#T##FederatedService.Protocol#>, authenticate: <#T##String#>, callback: <#T##String#>, completion: <#T##(Request, String) throws -> EventLoopFuture<ResponseEncodable>#>)
-    
-    //app.oAuth(from: <#T##FederatedService.Protocol#>, authenticate: <#T##String#>, authenticateCallback: <#T##((Request) throws -> (EventLoopFuture<Void>))?##((Request) throws -> (EventLoopFuture<Void>))?##(Request) throws -> (EventLoopFuture<Void>)#>, callback: <#T##String#>, scope: <#T##[String]#>, redirect: <#T##String#>)
-
-    
     try! app.register(collection: OAuthPg())
     
-    //HTTPCookies(dictionaryLiteral: ["test", "234"])
-    
-   // app.grouped(<#T##middleware: [Middleware]##[Middleware]#>)
-    
-    try app.oAuth(from: GitHub.self, authenticate: "github", callback: "") { (request, token) in
-        print("auth token: \(token)")
-        print("request: \(request.description)")
-        return request.eventLoop.makeSucceededFuture(Response())
-        //return request.eventLoop.future(request.redirect(to: "/"))
-    }
+
 }
