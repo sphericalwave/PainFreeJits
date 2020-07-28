2a91ee53d6181af7545f1396c9535559e60c8c32
aaronium112 <aaron@sphericalwave.com>
2020-07-28 17:00:45 -0300

    aweber script doesn't work. changes to discover categories on deep dive page.

---
+++ aweberAuth.sh
@@ -0,0 +1,2 @@
+curl -X POST \
+  https://auth.aweber.com/oauth2/authorize?response_type=code&client_id=AC8oAVWx08okvs6srOdsnr35ml3mAl4e&redirect_uri=http://localhost:8080&scope='subscriber.write'&state=randomStringOrSessionCookieHash
